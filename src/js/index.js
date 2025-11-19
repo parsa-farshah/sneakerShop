b8b0169bd36a3cfec7bf615fe4868925fbabbe74
parsa <parsafarashah@gmail.com>
2025-11-20 00:46:41 +0330

    feat: add theme in cookie

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,24 @@
 let $html = document.documentElement;
 let $darkLightBtn = document.getElementById("darkLightBtn");
 
+let $theme = Cookies.get("theme");
+
+if ($theme == "black") {
+  $html.classList.add("dark");
+} else {
+  $html.classList.remove("dark");
+}
+
+let flag = 0;
 $darkLightBtn.addEventListener("click", () => {
   $html.classList.toggle("dark");
+  flag++;
+
+  if (flag % 2 == 0) {
+    Cookies.set("theme", "white", { expires: 7 });
+    $html.classList.remove("dark");
+  } else {
+    Cookies.set("theme", "black", { expires: 7 });
+    $html.classList.add("dark");
+  }
 });

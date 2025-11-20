54b09882426ea7ec0e1158dccd9b189472ca52c8
parsa <parsafarashah@gmail.com>
2025-11-20 18:40:35 +0330

    feat: add mockapi sign up

--- src/js/index.js
+++ src/js/index.js
@@ -32,3 +32,50 @@ $profileBtn.addEventListener("click", () => {
   $profile.classList.remove("hidden");
   $profile.classList.add("flex");
 });
+
+// sign up
+
+let $btnSignUp = document.querySelector("#btnSignUp");
+let $signUpInputs = document.querySelectorAll("#signUpWrapper>div>input");
+let $userNameWrapper = document.querySelector("#userNameWrapper");
+
+$btnSignUp.addEventListener("click", () => {
+  console.log($signUpInputs);
+  let $SignUpName = $signUpInputs[0].value;
+  let $SignUpEmail = $signUpInputs[1].value;
+  let $SignUpPassword = $signUpInputs[2].value;
+
+  // signup add to mock api
+  const newTask = {
+    name: $SignUpName,
+    email: $SignUpEmail,
+    password: $SignUpPassword,
+  };
+
+  fetch("https://6912e51452a60f10c8232605.mockapi.io/users", {
+    method: "POST",
+    headers: { "content-type": "application/json" },
+    // Send your data in the request body as JSON
+    body: JSON.stringify(newTask),
+  })
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      // handle error
+    })
+    .then((task) => {
+      // do something with the new task
+      $userNameWrapper.innerText = "Hi," + task.name;
+
+      // go to main page
+      $profile.classList.remove("flex");
+      $main.classList.remove("hidden");
+      $main.classList.add("block");
+
+      
+    })
+    .catch((error) => {
+      // handle error
+    });
+});

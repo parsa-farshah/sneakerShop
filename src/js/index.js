19e7e2f9e44069b66a8cd900dfdd1313202c6945
parsa <parsafarashah@gmail.com>
2025-11-23 19:52:34 +0330

    feat: add trash for after cookie load cart products

--- src/js/index.js
+++ src/js/index.js
@@ -87,6 +87,7 @@ if (cartsCookie != undefined) {
               <h5 class="font-bold model">${item.model}</h5>
               <span class="text-sm price">${item.price}</span>
             </div>
+            <svg onclick=trash(this) xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" class="mt-4 cursor-pointer hover:scale-125 duration-300"><path fill="#000000" d="m6 2l2-2h4l2 2h4v2H2V2h4zM3 6h14l-1 14H4L3 6zm5 2v10h1V8H8zm3 0v10h1V8h-1z" class="dark:fill-white"/></svg>
           </div>`;
   });
 }
